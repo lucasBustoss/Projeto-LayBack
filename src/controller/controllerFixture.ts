56ff95629bf7e5d76e20efaa181e85ed431e5db6
Lucas Bustos <lbustos.silva@icloud.com>
2023-09-09 10:47:19 -0300

    feat: live fixtures route

--- src/controller/controllerFixture.ts
+++ src/controller/controllerFixture.ts
@@ -23,6 +23,25 @@ class ControllerFixture {
       return res.json(error(400, err))
     }
   }
+
+  async loadLiveFixtures (req: Request, res: Response): Promise<Response<HttpResponse>> {
+    try {
+      const { leagueId, homeTeamId, awayTeamId, initialDate, finalDate } = req.query
+      const filters = getFilters(
+        Number(leagueId), 
+        Number(homeTeamId), 
+        Number(awayTeamId), 
+        initialDate ? initialDate.toString() : null, 
+        finalDate ? finalDate.toString() : null)
+
+      const fixtures =  await serviceFixture.getLiveFixtures(filters)
+      
+      return res.json(ok(fixtures))
+    } catch (err) {
+      console.log(err.message)
+      return res.json(error(400, err))
+    }
+  }
 }
 
 export default new ControllerFixture()
