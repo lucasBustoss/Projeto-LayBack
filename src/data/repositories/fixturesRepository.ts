7b133ad566061333517eb309b84f51aec31307d1
Lucas Bustos <lbustos.silva@icloud.com>
2023-09-10 13:53:07 -0300

    feat: treat unnecessary fields on db search

--- src/data/repositories/fixturesRepository.ts
+++ src/data/repositories/fixturesRepository.ts
@@ -1,12 +1,16 @@
 import { FixturePastFilters } from "@/models/filters";
 import Fixture from "@/models/Fixture";
 import { FixtureSchema } from "@/models/schemas/schemaFixture";
+import { removeUnnecessaryFields } from "@/utils/helpers/mongo";
 
 class RepositoryFixture {
   async getPastFixtures(filters: FixturePastFilters): Promise<Fixture[]> {
-    const fixtures = await FixtureSchema.find(filters)
+    const documents = await 
+      FixtureSchema
+      .find(filters)
+      .select(removeUnnecessaryFields())
 
-    return fixtures
+    return documents
   }
 
   async save(fixtures: Fixture[]): Promise<void> {
---
+++ src/utils/helpers/mongo.ts
@@ -0,0 +1,3 @@
+export const removeUnnecessaryFields = (): string => {
+  return '-_id -__v -awayTeam._id -homeTeam._id -league._id -createdAt -updatedAt'
+}
