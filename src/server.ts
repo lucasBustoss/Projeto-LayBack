5bfe051e026ac14f2c9c168a6574fd95c229f15e
Lucas Bustos <lbustos.silva@icloud.com>
2023-09-11 22:01:51 -0300

    build: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import './config/module-alias'
 import * as dotenv from 'dotenv';
+import cors from 'cors';
+
 const { specs, swaggerUi } = require('./config/swagger');
 
 const path = '.env';
@@ -12,6 +14,8 @@ import mongoose from 'mongoose'
 
 const app = express();
 
+app.use(cors())
+app.use(express.json())
 app.use('/api', routes);
 app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
 mongoose.connect(process.env.MONGO_URL)
